6b2c2d7cb5853f602d0be8104f16a81f5bd95a74
Christian Smith <christian.46.smith@gmail.com>
2022-10-13 21:45:07 -0400

    Setting up login page and being setting up profile route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,12 +5,25 @@ const router = require('express').Router();
 
 router.get('/', withAuth, async (req, res) => {
     try {
-        const blogData = BlogPost.findAll();
+        req.session.loggedIn;
+        const blogData = await BlogPost.findAll();
         console.log(blogData);
-        res.render('homepage');
+        res.render('homepage', {
+            logged_in: req.session.logged_in
+        });
     } catch (err) {
         res.json(err);
     }
+});
+
+router.get('/login', (req, res) => {
+    res.render('login', {
+        logged_in: false
+    });
+});
+
+router.get('/profile', (req, res) => {
+    res.render('profile');
 });
 
 module.exports = router;
